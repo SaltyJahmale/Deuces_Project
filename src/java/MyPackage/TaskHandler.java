ca716ef733bd9ebec9d8cca2d7ac28a11ab326bd
Sharelison <sharelison@hotmail.com>
2015-05-13 21:22:57 +0200

    video spelen muziek spelen updated

--- src/java/MyPackage/TaskHandler.java
+++ src/java/MyPackage/TaskHandler.java
@@ -1,20 +1,16 @@
 import java.awt.Desktop;
 import java.io.File;
-import java.io.FileInputStream;
 import java.net.URI;
-
-import com.oracle.jrockit.jfr.DataType;
-
-import javazoom.jl.player.Player;
 
 
 public class TaskHandler {
 
-	private URI youtubeUrl = URI.create("https://www.youtube.com/watch?v=S_xH7noaqTA&list=RDS_xH7noaqTA");
+private URI youtubeUrl = URI.create("https://www.youtube.com/watch?v=S_xH7noaqTA&list=RDS_xH7noaqTA");
 	private String searchQuery = "";
 	private URI googleUrl = URI.create("https://www.google.nl/search?q=" + searchQuery + "&oq=" + searchQuery);
-	private String mainPath = "C:\\Users\\sharelison\\Music\\";
-	private String song = "Dreams.mp3";	    
+	private String localMainMusicPath = "C:\\Users\\sharelison\\Music\\";
+	private String localMainVideoPath = "C:\\Users\\sharelison\\Videos\\";
+	private String song = "Dreams.mp3";	 
 	
 	public void playYoutube() {
 		//Desktop d = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
@@ -23,7 +19,7 @@ public class TaskHandler {
 			try {
 				d.browse(youtubeUrl);
 			} catch(Exception e) {
-				
+				System.out.println("Exception at playYoutube");
 			}
 		}
 	}
@@ -42,61 +38,81 @@ public class TaskHandler {
 		}
 	}
 	
-	public void playMusic() {
-		try {    
-			String songFile = mainPath + song;
-	        FileInputStream in = new FileInputStream(new File(songFile));
-	        Player playMp3 = new Player(in);	        
-	        System.out.println("playing...");
-	        playMp3.play();
-			System.out.println("played song successfully!");
-
+	//set to only play mp3 files
+	public void playSong(String songName) {
+		if(!isMp3(songName)) {
+			song = songName + ".mp3";
+		}
+		
+		Desktop d = Desktop.getDesktop();
+		try {
+			if(d != null && d.isSupported(Desktop.Action.OPEN)) {
+				d.open(new File(localMainMusicPath + song));
+			}
 		} catch(Exception e) {
-	        System.out.println("songFile");
-
+			System.out.println("Exception at playSong");
 			e.printStackTrace();
 		}
 	}
 	
+	//set to only play mp3 files
 	public void playSongAtPath(String songPath) {	
-     FileInputStream fileInStream;
-	 try {
-		if(isMp3(songPath)) {
-	        fileInStream = new FileInputStream(new File(songPath));
-		} else {
-			fileInStream = new FileInputStream(new File(songPath + ".mp3"));
-		}
-		
-        Player playMp3 = new Player(fileInStream);	        
-        System.out.println("playing...");
-        playMp3.play();
-		System.out.println("played song successfully!");
+	     File file;
+		 try {
+			if(isMp3(songPath)) {
+		        file = new File(songPath);
+			} else {
+				file = new File(songPath + ".mp3");
+			}
+			
+			Desktop d = Desktop.getDesktop();
+			if (d != null && d.isSupported(Desktop.Action.OPEN)) {
+				d.open(file);
+			}
 
-	 } catch(Exception e) {
-        System.out.println("songFile");
-		e.printStackTrace();
-	 }
-	
+		 } catch(Exception e) {
+	        System.out.println("Exception at playSongAtPath");
+			e.printStackTrace();
+		 }
+		
 	}
 	
-	public void setUrl(String url) {
-		this.youtubeUrl = URI.create(url);
-	}
-	
-	public void updateGoogleUrl() {
-		googleUrl = URI.create("https://www.google.nl/search?q=" + searchQuery + "&oq=" + searchQuery);
-	}
-	
-	public void setSong(String songName) {
-		if (isMp3(songName)) {
-			song = songName;
-		} else {
-			song = songName + ".mp3";
+	public void playVideo(String video) {	
+		
+		/* videonames without given extension(videoname instead of videoname.mp4) can only be played if it's an mp4 file
+		 * otherwise exception raised.
+		 */		
+		if(!hasExtension(video)){
+			video = video + ".mp4";
+		} 
+		
+		Desktop d = Desktop.getDesktop();
+		if(d != null && d.isSupported(Desktop.Action.OPEN)) {
+			try {
+				File file = new File(localMainVideoPath + video);
+				d.open(file);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
-	public void setMainPath(String path) {
-		mainPath = path;
+	public void playVideoAtPath(String videoUrl) {
+			/* videonames without given extension(videoname instead of videoname.mp4) can only be played if it's an mp4 file
+			 * otherwise exception raised.
+			 */		
+			if(!hasExtension(videoUrl)){
+				videoUrl = videoUrl + ".mp4";
+			} 
+			
+			Desktop d = Desktop.getDesktop();
+			if(d != null && d.isSupported(Desktop.Action.OPEN)) {
+				try {
+					File file = new File(videoUrl);
+					d.open(file);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
 	}
 	
 	private boolean isMp3(String songName) {
@@ -107,19 +123,29 @@ public class TaskHandler {
 		}
 	}
 	
-	public void playVideo(String videoUrl) {
-			Desktop d = Desktop.getDesktop();
-			if(d.isSupported(Desktop.Action.BROWSE)) {
-				
-				try {
-					URI url = new URI(videoUrl);
-					d.browse(url);
-
-				} catch(Exception e) {
-					e.printStackTrace();
-
-				}
-			}
+	public void setYoutubeUrl(String url) {
+		this.youtubeUrl = URI.create(url);
 	}
+	
+	private void updateGoogleUrl() {
+		googleUrl = URI.create("https://www.google.nl/search?q=" + searchQuery + "&oq=" + searchQuery);
+	}
+	
+	public void setMusicMainPath(String path) {
+		localMainMusicPath = path;
+	}
+	
+	public void setVideoMainPath(String path) {
+		localMainVideoPath = path;
+	}
+	
+	private boolean hasExtension(String filename) {
+		if(filename.substring(filename.length() - 4, filename.length() - 3).equals(".")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
 
